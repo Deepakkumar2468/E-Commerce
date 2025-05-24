f070a34db4d07b5964a81c76aa48a47d2d2c37ec
[Deepak kumar] <[kdeepak41369@gmail.com]>
2025-05-24 23:44:37 +0530

    project completed

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+
+const authUser = async (req, res, next) => {
+  const { token } = req.headers;
+
+  if (!token) {
+    return res.json({ success: false, message: 'Not authorized login again' });
+  }
+
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { id: token_decode.id }; // ✅ Set req.user
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default authUser;
